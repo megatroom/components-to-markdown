db568784babfef93cc1843782e6cd6cf2671baff
Bruno Nardini <megatroom3@gmail.com>
2022-09-12 20:13:21 -0300

    🎨 Adjustment of doc layout responsiveness

--- packages/website/docs/contribute/how-to-contribute.md
+++ packages/website/docs/contribute/how-to-contribute.md
@@ -12,25 +12,29 @@ C2M is a open source project that welcomes contributions.
 
 ## Get involved
 
-<div style={{ display: 'flex', justifyContent: 'space-around', textAlign: 'center' }}>
-  <div style={{ maxWidth: '300px' }}>
-    <div style={{ display: 'inline-block', height: '160px' }}>
-      <TalkSvg />
+<div class="row" style={{ textAlign: 'center' }}>
+  <div class="col col--6">
+    <div style={{ maxWidth: '300px', margin: '0 auto' }}>
+      <div style={{ display: 'inline-block', height: '160px' }}>
+        <TalkSvg />
+      </div>
+      <h2>Talk about C2M</h2>
+      <p>
+        The easiest way to get involved is to share C2M with fellow developers, colleagues, and friends.
+      </p>
     </div>
-    <h2>Talk about C2M</h2>
-    <p>
-      The easiest way to get involved is to share C2M with fellow developers, colleagues, and friends.
-    </p>
   </div>
-  <div style={{ maxWidth: '300px' }}>
-    <div style={{ display: 'inline-block', height: '164px' }}>
-      <GithubSvg  />
+  <div class="col col--6">
+    <div style={{ maxWidth: '300px', margin: '0 auto' }}>
+      <div style={{ display: 'inline-block', height: '164px' }}>
+        <GithubSvg  />
+      </div>
+      <h2>Star on GitHub</h2>
+      <p>
+        <a href="https://github.com/megatroom/components-to-markdown" aria-label="Star megatroom/components-to-markdown on GitHub" target="_blank">Star this project on GitHub</a><br />
+        and help the project to stand out.
+      </p>
     </div>
-    <h2>Star on GitHub</h2>
-    <p>
-      <a href="https://github.com/megatroom/components-to-markdown" aria-label="Star megatroom/components-to-markdown on GitHub" target="_blank">Star this project on GitHub</a><br />
-      and help the project to stand out.
-    </p>
   </div>
 </div>
 
--- packages/website/src/components/HowItWorks/index.tsx
+++ packages/website/src/components/HowItWorks/index.tsx
@@ -28,66 +28,83 @@ export default function HowItWorks(): JSX.Element {
   return (
     <section className={styles.section}>
       <div className="container">
-        <h2 className="margin-bottom--lg text--center">How it works</h2>
+        <h2
+          className={clsx(
+            'margin-bottom--lg text--center',
+            styles.sectionTitle
+          )}
+        >
+          How it works
+        </h2>
       </div>
-      <div className={styles.previewTable}>
-        <div>
-          <h3>1. Write Your Component</h3>
-          <p>You will develop your code in the conventional way using types:</p>
-          <CodeBlock language="tsx" title="Button.tsx">
-            {ComponentExample}
-          </CodeBlock>
-        </div>
-        <div className="margin-bottom--lg">
-          <h3>2. Choose a template</h3>
-          <p>
-            Then you can create your own{' '}
-            <a href={useBaseUrl('/docs/getting-started/template')}>template</a>{' '}
-            or select one of the available templates:
-          </p>
-          <div className="margin-bottom--lg">
-            <div className="dropdown dropdown--hoverable">
-              <button className="button button--primary">
-                {template.label}
-              </button>
-              <ul className="dropdown__menu">
-                {templateNames.map((option) => (
-                  <li>
-                    <a
-                      className={clsx('dropdown__link', {
-                        'dropdown__link--active':
-                          option.value === template.value,
-                      })}
-                      href="#"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setTemplate(option);
-                      }}
-                    >
-                      {option.label}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+      <div className={styles.fullWidthContainer}>
+        <div className="row margin-bottom--lg">
+          <div className="col col--6">
+            <div className={styles.howItWorkColumn}>
+              <h3>1. Write Your Component</h3>
+              <p>
+                You will develop your code in the conventional way using types:
+              </p>
+              <CodeBlock language="tsx" title="Button.tsx">
+                {ComponentExample}
+              </CodeBlock>
             </div>
           </div>
-          <h3>3. Generate the Documentation</h3>
-          <p>
-            Now just{' '}
-            <a href={useBaseUrl('/docs/getting-started/introduction#usage')}>
-              generate your awesome documentation
-            </a>
-            .
-          </p>
-          <p>Below is a demo according to the template chosen above:</p>
-          <BrowserWindow
-            minHeight={400}
-            url="https://your-awesome-documentation.com"
-          >
-            <div className={styles.previewBody}>
-              <ButtonDoc />
+          <div className="col col--6">
+            <div className={styles.howItWorkColumn}>
+              <h3>2. Choose a template</h3>
+              <p>
+                Then you can create your own{' '}
+                <a href={useBaseUrl('/docs/getting-started/template')}>
+                  template
+                </a>{' '}
+                or select one of the available templates:
+              </p>
+              <div className="margin-bottom--lg">
+                <div className="dropdown dropdown--hoverable">
+                  <button className="button button--primary">
+                    {template.label}
+                  </button>
+                  <ul className="dropdown__menu">
+                    {templateNames.map((option) => (
+                      <li>
+                        <a
+                          className={clsx('dropdown__link', {
+                            'dropdown__link--active':
+                              option.value === template.value,
+                          })}
+                          href="#"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setTemplate(option);
+                          }}
+                        >
+                          {option.label}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <h3>3. Generate the Documentation</h3>
+              <p>
+                Now just{' '}
+                <a
+                  href={useBaseUrl('/docs/getting-started/introduction#usage')}
+                >
+                  generate your awesome documentation
+                </a>
+                .
+              </p>
+              <p>Below is a demo according to the template chosen above:</p>
+              <BrowserWindow
+                minHeight={400}
+                url="https://your-awesome-documentation.com"
+              >
+                <ButtonDoc />
+              </BrowserWindow>
             </div>
-          </BrowserWindow>
+          </div>
         </div>
       </div>
       <div className="container text--center">
