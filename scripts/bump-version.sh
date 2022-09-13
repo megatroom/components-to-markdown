ea9d4aa063bf2baa7572f5cde2a97ded3eee0cc3
Bruno Nardini <megatroom3@gmail.com>
2022-09-13 01:51:36 -0300

    🔧 Remove label release

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -3,11 +3,14 @@ export PATH=$(npm bin):$PATH
 DIST_TAG=$1
 VERSION=`yarn -s auto version`
 
+echo ""
+
 if [ ! -z "$VERSION" ]; then
-  echo "\nUpdating changelog..."
+  echo "Updating changelog..."
   yarn auto changelog --dry-run
 
-  echo "\nBumping $VERSION version..."
+  echo ""
+  echo "Bumping $VERSION version..."
   cd packages/components-to-markdown
   TAG_NAME=$(npm version $VERSION -m "[skip ci] Bump version to: %s" --no-git-tag-version)
   echo "New tag: $TAG_NAME"
@@ -15,19 +18,23 @@ if [ ! -z "$VERSION" ]; then
   cd -
   echo "New version: $VERSION_NUMBER"
 
-  echo "\nPublishing package..."
+  echo ""
+  echo "Publishing package..."
   echo "NPM dist tag: $DIST_TAG"
   # yarn nx publish components-to-markdown --tag=$DIST_TAG --ver=$VERSION_NUMBER
 
 
-  echo "\nCreating GitHub Release..."
+  echo ""
+  echo "Creating GitHub Release..."
   # git push --follow-tags --set-upstream origin ${CIRCLE_BRANCH}
   yarn auto release --dry-run
 
 else
 
-  echo "\nNo new version defined. Skipping release."
+  echo "No new version defined. Skipping release."
 
 fi
 
-echo "\nDone.\n"
+echo ""
+echo "Done."
+echo ""
