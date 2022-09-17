61ef5b9ab6f69ad19ddb3044397dd3748143ed18
Bruno Nardini <megatroom3@gmail.com>
2022-09-17 17:32:09 -0300

    🔨 Change check-git-diff logic to work on CircleCI

--- scripts/check-git-diff.sh
+++ scripts/check-git-diff.sh
@@ -1,7 +1,21 @@
-CHANGES=$(git diff-index --name-status HEAD)
+DIFF_PATH=$1
 
-if [ -n "$CHANGES" ]; then
-    echo "You have uncommitted changes:"
-    echo "$CHANGES"
-    exit 1
+echo ""
+
+if [ -z "$DIFF_PATH" ]; then
+  echo "Error: You must specify the path to the diff."
+  echo ""
+  exit 1
 fi
+
+git diff --name-status --exit-code $DIFF_PATH
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo ""
+  echo "ERROR: running the previous command has introduced changes. Hence, Failing the build."
+  echo ""
+  exit 1
+fi
+
+echo "Done."
+echo ""
