92ed8f75c4f504ebafa9f214e3e44b70adebf7cf
Bruno Nardini <megatroom3@gmail.com>
2022-09-19 23:12:02 -0300

    📝 Update demonstration structure

--- scripts/comp-to-website.sh
+++ scripts/comp-to-website.sh
@@ -11,12 +11,12 @@ yarn
 cd -
 
 ./dist/packages/components-to-markdown/bin/components-to-markdown \
-  --output packages/website/demo/brachiosaurus \
+  --output packages/website/demo/brachiosaurus/demonstration \
   --loglevel debug \
   packages/example-react-components/src/lib packages/example-prop-types/src/lib
 
 ./dist/packages/components-to-markdown/bin/components-to-markdown \
-  --output packages/website/demo/stegosaurus \
+  --output packages/website/demo/stegosaurus/demonstration \
   --loglevel debug \
   --template stegosaurus \
   packages/example-react-components/src/lib packages/example-prop-types/src/lib
